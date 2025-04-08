6ac5a51aad02ec0eccfefba1af54fba75def74f1
alinen <anormoyle@brynmawr.edu>
2025-04-08 18:47:01 -0400

    tweak writup

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
+/usr/local/bin/asciidoctor assts/*.adoc *.adoc
 if [ $# -ne 1 ]; then 
   echo "usage: ./build.sh <message>"
 fi
 
-/usr/local/bin/asciidoctor assts/*.adoc *.adoc
 git add .
 git commit -m "$1" 
